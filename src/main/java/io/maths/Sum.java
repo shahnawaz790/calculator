0dac07b4b09c49026b00f7ea846c156f4d00d7d7
ShahnawazEstater <shahnawaz@estater.com>
2020-08-03 03:27:17 +0530

    Created maven project in eclipse IDE and Created a class Sum and SumTest for  Junit test cases

---
+++ src/main/java/io/maths/Sum.java
@@ -0,0 +1,5 @@
+package io.maths;
+
+public class Sum {
+	
+}
---
+++ src/test/java/io/maths/SumTest.java
@@ -0,0 +1,11 @@
+package io.maths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SumTest {
+	
+	
+}
